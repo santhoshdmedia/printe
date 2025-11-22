fa267218d4ea0bc75437a3f050529664e3258668
vikash050 <vikashramdoss5@gmail.com>
2025-11-22 15:10:13 +0530

    changed url

--- live/src/pages/shopping/pay/utils/payment.js
+++ live/src/pages/shopping/pay/utils/payment.js
@@ -1,6 +1,7 @@
 export const initiateCCAvenuePayment = async (paymentData) => {
   try {
-    const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+    const apiUrl = import.meta.env.VITE_BASE_API_URL || 'http://localhost:8080';
+    
 
     const response = await fetch(`${apiUrl}/api/payment/create-order`, {
       method: 'POST',
