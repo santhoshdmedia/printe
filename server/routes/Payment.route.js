27c619dd485e421710f462fcc21d642505de8570
vikash050 <vikashramdoss5@gmail.com>
2025-12-16 10:53:18 +0530

    changed payment Route

--- server/routes/Payment.route.js
+++ server/routes/Payment.route.js
@@ -1,24 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {
-  applyCoupon,
-  createCoupon,
-  getAllCoupons,
-  getCouponById,
-  updateCoupon,
-  deleteCoupon,
-  markCouponAsUsed
-} = require('../controller/coupen.controller');
+const { createPaymentOrder, handleCCAvenueCallback } = require('../controller/Payment.controller');
 
-// Apply coupon (Public - but requires user authentication in production)
-router.post('/apply', applyCoupon);
-
-// Admin routes (Add authentication middleware in production)
-router.post('/', createCoupon);
-router.get('/', getAllCoupons);
-router.get('/:id', getCouponById);
-router.put('/:id', updateCoupon);
-router.delete('/:id', deleteCoupon);
-router.post('/mark-used', markCouponAsUsed);
+router.post('/create-order', createPaymentOrder);
+router.post('/ccavenue/callback', handleCCAvenueCallback);
 
 module.exports = router;
