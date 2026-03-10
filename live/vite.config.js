0d0b8b45cb06438dea71a35cf9be4b81044a4011
santhoshdmedia <devconsoledmedia@gmail.com>
2026-03-10 11:52:08 +0530

    changed vite

--- live/vite.config.js
+++ live/vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Core React
+          'react-core': ['react', 'react-dom'],
+          'react-router': ['react-router-dom'],
+
+          // HTTP / State
+          'axios': ['axios'],
+          'redux': ['redux', '@reduxjs/toolkit', 'react-redux'],
+
+          // UI libraries (add whatever you use)
+          'antd': ['antd'],
+          'mui': ['@mui/material', '@mui/icons-material'],
+          'icons': ['react-icons'],
+        }
+      }
+    },
+    // Compress assets
+    assetsInlineLimit: 4096,      // inline files < 4kb
+    cssCodeSplit: true,
+    sourcemap: false,             // disable sourcemaps in prod (saves size)
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,       // remove all console.logs in prod
+        drop_debugger: true,
+      }
+    }
+  }
 })
