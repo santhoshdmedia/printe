d08a61522d25e724aed4f27b770d25f3c2712f60
santhoshdmedia <devconsoledmedia@gmail.com>
2026-03-11 12:03:42 +0530

    changed index

--- server/index.js
+++ server/index.js
@@ -35,7 +35,6 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
   res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader("X-Content-Type-Options", "nosniff");
-  res.setHeader("X-Frame-Options", "DENY");
   res.setHeader("X-XSS-Protection", "1; mode=block");
   res.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
 
